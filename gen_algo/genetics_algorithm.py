39f06db7d83664df88908ade345bb9694b6fcf42
Lukáš Hejnal <kejhy93@gmail.com>
2026-07-14 12:24:06 +0200

    Reduce cognitive complexity of GeneticsAlgorithm.solve
    Extract the per-generation body into run_generation, and the nested
    hill-climbing and simulated-annealing blocks into their own methods.
    solve() itself is now just the population setup and the generation
    loop; timing collection, print output, and step ordering are
    unchanged.
    Fixes SonarCloud python:S3776 at gen_algo/genetics_algorithm.py:71.

--- gen_algo/genetics_algorithm.py
+++ gen_algo/genetics_algorithm.py
@@ -84,119 +84,140 @@ class GeneticsAlgorithm(AbstractSolver):
         if self.verboseGeneticsSolver:
             print(" Init population: ", population)
 
+        for iteration in range(1, self.MAX_GENERATION + 1):
+            population, best_individual_of_population = self.run_generation(population, iteration,
+                                                                            best_individual_of_population)
+
+        return best_individual_of_population.get_individual()
+
+    def run_generation(self, population, iteration, best_individual_of_population):
+        """
+        Run mutation, crossover, hill-climbing, simulated annealing, and
+        ant-colony for a single generation, then track timing and the
+        best individual found so far
+        """
         IS_MUTATION = True
         IS_CROSSOVER = True
         IS_HILL_CLIMBING = False
         IS_SIMULATED_ANNEALING = True
         IS_ANT_COLONY = True
 
-        for iteration in range(1, self.MAX_GENERATION + 1):
-            iterationStr = ""
-            iterationStr = "Iteration: " + str(iteration) + "\n"
+        start_times = []
+        end_times = []
+        methods = []
 
-            start_times = []
-            end_times = []
+        start_times.append(utils.get_time_in_millis())
+        # MUTATION
+        if IS_MUTATION:
+            print("GeneticsAlgorithm -> Mutation")
+            self.mutate(population, iteration)
 
-            times = []
-            methods = []
+        methods.append("Mutation")
+        end_times.append(utils.get_time_in_millis())
 
-            start_times.append(utils.get_time_in_millis())
-            # MUTATION
-            if IS_MUTATION:
-                print("GeneticsAlgorithm -> Mutation")
-                self.mutate(population, iteration)
+        # CROSS-OVER
+        start_times.append(utils.get_time_in_millis())
 
-            methods.append("Mutation")
-            end_times.append(utils.get_time_in_millis())
+        crossover_probability = random.random()
+        if IS_CROSSOVER and crossover_probability < self.CROSSOVER_RATE:
+            print("GeneticsAlgorithm -> Crossover")
+            population = self.do_crossover(population, self.COUNT_OF_CROSSOVER_PER_GENERATION)
+        methods.append("Crossover")
+        end_times.append(utils.get_time_in_millis())
 
-            # CROSS-OVER
-            start_times.append(utils.get_time_in_millis())
+        # HILL-CLIMBING
+        start_times.append(utils.get_time_in_millis())
 
-            crossover_probability = random.random()
-            if IS_CROSSOVER and crossover_probability < self.CROSSOVER_RATE:
-                print("GeneticsAlgorithm -> Crossover")
-                population = self.do_crossover(population, self.COUNT_OF_CROSSOVER_PER_GENERATION)
-            methods.append("Crossover")
-            end_times.append(utils.get_time_in_millis())
+        if IS_HILL_CLIMBING:
+            print("GeneticsAlgorithm -> Hill-Climbing")
+            population = self.run_hill_climbing_step(population, iteration)
 
-            # HILL-CLIMBING
-            start_times.append(utils.get_time_in_millis())
+        methods.append("Hill-Climbing")
+        end_times.append(utils.get_time_in_millis())
 
-            if IS_HILL_CLIMBING:
-                print("GeneticsAlgorithm -> Hill-Climbing")
-                if iteration % self.FREQUANCY_OF_HILL_CLIMBING == 0:
-                    # Pick random individual from population
-                    parent, index_of_parent = population.pick_random_individual()
-                    # Hill-Climbing
-                    mutated_individual = do_hill_climbing(parent, HILL_CLIMBING_COUNT_OF_ITERATION,
-                                                          HILL_CLIMBING_COUNT_OF_NEIGHOUR)
-                    # New individual to population
-                    population.set_individual_at(index_of_parent, mutated_individual)
+        # SIMULATED ANNEALING
+        start_times.append(utils.get_time_in_millis())
 
-            methods.append("Hill-Climbing")
-            end_times.append(utils.get_time_in_millis())
+        if IS_SIMULATED_ANNEALING:
+            print("GeneticsAlgorithm -> Simulated Annealing")
+            population = self.run_simulated_annealing_step(population)
 
-            # SIMULATED ANNEALING
-            start_times.append(utils.get_time_in_millis())
+        methods.append("Simulated Annealing")
+        end_times.append(utils.get_time_in_millis())
 
-            if IS_SIMULATED_ANNEALING:
-                print("GeneticsAlgorithm -> Simulated Annealing")
-                # Get random unique indexes of individuals
-                index_of_individuals = random.sample(range(0, population.count_of_individuals()),
-                                                     COUNT_OF_SIMULATED_ANNEALING)
+        # ANT-COLONY
+        start_times.append(utils.get_time_in_millis())
 
-                # # Fill individuals
-                individuals_to_simulated_annealing = []
-                for index in index_of_individuals:
-                    individuals_to_simulated_annealing.append(population.get_individual_at(index))
+        if IS_ANT_COLONY:
+            print("GeneticsAlgorithm -> Ant-Colony")
+            population = self.do_ant_colony(population, iteration)
 
-                # # Init thread pool
-                simulated_annealing_pool = ThreadPool(8)
-                # # Simulated Annealing
-                results = simulated_annealing_pool.starmap(do_simulated_annealing,
-                                                           zip(individuals_to_simulated_annealing))
-                # # Replace new individuals to population
-                for mutated_individual, index in zip(results, index_of_individuals):
-                    population.set_individual_at(index, mutated_individual)
-                # Clear thread pool
-                simulated_annealing_pool.close()
+        methods.append("Ant-Colony")
+        end_times.append(utils.get_time_in_millis())
 
-            methods.append("Simulated Annealing")
+        time_to_print = utils.get_string_of_computed_times(start_times, end_times, methods)
 
-            end_times.append(utils.get_time_in_millis())
+        # Pick best individual from population and compare with best individual found
+        best_individual_of_iteration, average_fitness = population.pick_best_individual()
 
-            # ANT-COLONY
-            start_times.append(utils.get_time_in_millis())
+        energy_of_best_individual_of_iteration = best_individual_of_iteration.compute_free_energy()
 
-            if IS_ANT_COLONY:
-                print("GeneticsAlgorithm -> Ant-Colony")
-                population = self.do_ant_colony(population, iteration)
+        best_individual_of_population = self.get_best_individual(best_individual_of_iteration,
+                                                                 best_individual_of_population)
 
-            methods.append("Ant-Colony")
-            end_times.append(utils.get_time_in_millis())
+        if self.STORE_INDIVIDUALS_PER_GENERATION:
+            self.list_individuals.append(best_individual_of_iteration)
 
-            time_to_print = utils.get_string_of_computed_times(start_times, end_times, methods)
-
-            # Pick best individual from population and compare with best individual found
-            best_individual_of_iteration, average_fitness = population.pick_best_individual()
-
-            energy_of_best_individual_of_iteration = best_individual_of_iteration.compute_free_energy()
-
-            best_individual_of_population = self.get_best_individual(best_individual_of_iteration,
-                                                                     best_individual_of_population)
-
-            if self.STORE_INDIVIDUALS_PER_GENERATION:
-                self.list_individuals.append(best_individual_of_iteration)
-
-            iterationStr += "Energy of best individual: {:10.3f}, Average fitness: {:10.3f}".format(
+        # Print best individual
+        if self.verboseGeneticsSolver:
+            iteration_str = "Iteration: " + str(iteration) + "\n"
+            iteration_str += "Energy of best individual: {:10.3f}, Average fitness: {:10.3f}".format(
                 best_individual_of_population.get_free_energy(), average_fitness)
-            # Print best individual
-            if self.verboseGeneticsSolver:
-                print(time_to_print)
-                print(iterationStr)
-            # best_individual_of_population.get_individual().plot_config()
+            print(time_to_print)
+            print(iteration_str)
 
-        return best_individual_of_population.get_individual()
+        return population, best_individual_of_population
+
+    def run_hill_climbing_step(self, population, iteration):
+        """
+        Occasionally replace a random individual with a hill-climbed one
+        """
+        if iteration % self.FREQUANCY_OF_HILL_CLIMBING == 0:
+            # Pick random individual from population
+            parent, index_of_parent = population.pick_random_individual()
+            # Hill-Climbing
+            mutated_individual = do_hill_climbing(parent, HILL_CLIMBING_COUNT_OF_ITERATION,
+                                                  HILL_CLIMBING_COUNT_OF_NEIGHOUR)
+            # New individual to population
+            population.set_individual_at(index_of_parent, mutated_individual)
+
+        return population
+
+    def run_simulated_annealing_step(self, population):
+        """
+        Replace a random sample of individuals with simulated-annealed ones
+        """
+        # Get random unique indexes of individuals
+        index_of_individuals = random.sample(range(0, population.count_of_individuals()),
+                                             COUNT_OF_SIMULATED_ANNEALING)
+
+        # Fill individuals
+        individuals_to_simulated_annealing = []
+        for index in index_of_individuals:
+            individuals_to_simulated_annealing.append(population.get_individual_at(index))
+
+        # Init thread pool
+        simulated_annealing_pool = ThreadPool(8)
+        # Simulated Annealing
+        results = simulated_annealing_pool.starmap(do_simulated_annealing,
+                                                   zip(individuals_to_simulated_annealing))
+        # Replace new individuals to population
+        for mutated_individual, index in zip(results, index_of_individuals):
+            population.set_individual_at(index, mutated_individual)
+        # Clear thread pool
+        simulated_annealing_pool.close()
+
+        return population
 
     def mutate(self, population, iteration):
         index_of_individuals = random.sample(range(0, population.count_of_individuals()),
